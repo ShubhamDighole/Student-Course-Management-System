939ad3a7eb600fc7fc0012a8433cefff0510cb99
shubham <shubhamdighole@gmail.com>
2025-12-28 08:57:07 +0530

    Remove all files from main branch

--- src/com/airtribe/learntrack/customexception/StudentNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.airtribe.learntrack.customexception;
-
-public class StudentNotFoundException extends RuntimeException{
-
-	public StudentNotFoundException(int searchId) {
-		super("Student did not found with Student_id : " +searchId);
-	}
-}
--- src/com/airtribe/learntrack/entity/Trainee.java
+++
@@ -1,8 +0,0 @@
-package com.airtribe.learntrack.entity;
-
-public class Trainee extends Person{
-	
-	public String getDisplayName() {
-		return "From Trainee Class";
-	}
-}
